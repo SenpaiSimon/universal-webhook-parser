160b514131476d7121672782c8ce2bbcca600f32
SenpaiSimon <cosmonius.lp.so@googlemail.com>
2026-02-16 17:51:34 +0100

    [Feature] Create Incoming Hook Path and Schemas - Schemas for Tasks - Schemas for a Hook - Schemas for a Parser - Endpoints for Hooks

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,35 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const parser = sqliteTable('parser', {
+	id: text('id')
+			.primaryKey()
+			.$defaultFn(() => crypto.randomUUID()),
+	title: text('title').notNull(),
+	descrition: text('description'),
+	code: text('code').notNull()
+});
+
+export const hooks = sqliteTable('hooks', {
+	id: text('id')
+			.primaryKey()
+			.$defaultFn(() => crypto.randomUUID()),
+	title: text('title').notNull(),
+	descrition: text('description'),
+	parserId: text('parser_id')
+			.references(() => parser.id)
+			.notNull(),
+	outHookUrl: text('out_hook_url').notNull(),
+	outHookMethod: text('out_hook_method').notNull()
+});
 
 export const task = sqliteTable('task', {
 	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+			.primaryKey()
+			.$defaultFn(() => crypto.randomUUID()),
+	hookId: text('hook_id')
+			.references(() => hooks.id)
+			.notNull(),
+	startTime: text('start_time').$default(() => new Date().toISOString()),
+	endTime: text('end_time'),
+	status: text('status').notNull()
 });
